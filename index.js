cc5a49be466bb1238bb8695a41bb560bc5644282
Zainab Al-Najjar <eng.zainab.alnajar@gmail.com>
2024-03-14 16:37:24 +0300

    Completed assignment!

--- index.js
+++ index.js
@@ -17,10 +17,21 @@ function isPalindrome(word) {
 
 /* 
   Add your pseudocode here
+
+  iterate from the beginning of the string to the middle of the string
+  compare the letter we're iterating over to the corresponding letter at the end of the string
+    if the letters don't match, return false
+
+  if we reach the middle, and all the letters match, return true
 */
 
 /*
   Add written explanation of your solution here
+
+  1- iterate from the beginning of the string to the middle of the string
+  2- compare the letter we're iterating over to the corresponding letter at the end of the string
+  3- if the letters don't match, return false
+  4- if we reach the middle, and all the letters match, return true
 */
 
 // You can run `node index.js` to view these console logs
